131f11c0758b78b9966a67ab295bee9a295c25da
Gabriel Roa <geroa@uno.edu>
2016-11-28 20:34:42 -0600

    Added couple of files and creating new branch

--- app/src/main/java/com/stephenjackson/entertainmentengine/ResultsActivity.java
+++ app/src/main/java/com/stephenjackson/entertainmentengine/ResultsActivity.java
@@ -2,17 +2,63 @@ package com.stephenjackson.entertainmentengine;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.TextureView;
 import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import static com.stephenjackson.entertainmentengine.SearchActivity.entry;
 
 public class ResultsActivity extends AppCompatActivity {
     private TextView tView;
+    private TextView resultOne;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
         tView = (TextView) findViewById(R.id.textView);
         tView.setText(entry);
+        resultOne = (TextView) findViewById(R.id.result1);
+        resultOne.setText("Suh dude");
+        //String string = null;
+        try {
+            System.out.println(results());
+        } catch (JSONException e) {
+            e.printStackTrace();
+            System.out.println(e);
+        }
+
+        //System.out.println(string);
+        //handleJSON();
+    }
+
+    protected String results() throws JSONException{
+        String url = "http://api-public.guidebox.com/v1.43/US/rKIvHF0soparXENAMNBTfMrOBlJusc49/movie/50362";
+
+        JsonObjectRequest jsObjRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        System.out.println("Obtained response");
+                        System.out.println("Response: " + response.toString());
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO Auto-generated method stub
+                        System.out.println("Error dude");
+                        error.printStackTrace();
+                        System.out.println(error);
+
+                    }
+                });
+        return url;
     }
 }
